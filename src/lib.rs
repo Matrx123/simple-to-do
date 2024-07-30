34627b6772b9f26334468939c7f12390d735d4a6
vipin joshi <vpnjsh55@gmail.com>
2024-07-30 21:24:37 +0530

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,25 +2,33 @@ use std::collections::HashMap;
 
 pub struct Todo {
     list: HashMap<i32, String>,
-    curr_index: i32,
+    next_index: i32,
 }
 
 impl Todo {
     pub fn new() -> Todo {
         Todo {
             list: HashMap::new(),
-            curr_index: 0,
+            next_index: 0,
         }
     }
 
-    pub fn todo(self) -> HashMap<i32, String> {
-        self.list
+    pub fn get_all(&self) -> &HashMap<i32, String> {
+        &self.list
     }
 
     pub fn add(&mut self, items: String) -> &str {
-        self.list.insert(self.curr_index + 1, items);
-        self.curr_index += 1;
+        self.list.insert(self.next_index, items);
+        self.next_index += 1;
         "todo item added!"
+    }
+
+    pub fn delete(&mut self, id: i32) -> Result<usize, &str> {
+        if self.list.remove(&id).is_some() {
+            Ok(self.list.len())
+        } else {
+            Err("Item not found!!")
+        }
     }
 }
 
@@ -28,14 +36,47 @@ impl Todo {
 mod tests {
     use super::*;
 
+    fn setup() -> Todo {
+        let mut list = Todo::new();
+        list.add(String::from("apple"));
+        list.add(String::from("banana"));
+        list.add(String::from("guava"));
+        list.add(String::from("grapes"));
+        list
+    }
+
     #[test]
     fn add_todo_items_to_a_list() {
         let mut list = Todo::new();
         list.add(String::from("apple"));
         list.add(String::from("banana"));
-        list.add(String::from("guvava"));
+        list.add(String::from("guava"));
         list.add(String::from("grapes"));
-        let result = list.curr_index;
+
+        let result = list.get_all().len();
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn fetch_all_todo_items() {
+        let list = setup();
+        let result = list.get_all().len();
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn delete_an_item_from_list() {
+        let mut list = setup();
+        let result = list.delete(2);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 3);
+    }
+
+    #[test]
+    fn delete_non_existing_item() {
+        let mut list = setup();
+        let result = list.delete(10);
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), "Item not found!!");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,14 @@ use todo_app::Todo;
 //Simple ToDo cli in RUST
 fn main() {
     println!("CLI for TODO App 🦀");
+    println!("enter a todo command or '--help' for todo commands!");
     println!("--------------------");
     let args: Vec<String> = env::args().collect();
-    println!("args >>> {:?}", args);
-    println!("enter a todo command or '--help' for todo commands!");
+    if args.len() > 1 && args[1] == "help" {
+        println!("Todo commands:\n1- add 'task name'\n2- delete 'todo item id'\n3- update 'item id' 'updated text'");
+        println!("--------------------");
+    }
+
     let mut input = String::new();
 
     io::stdin()
@@ -19,6 +23,6 @@ fn main() {
 
     let mut list = Todo::new();
     list.add(String::from("vipin joshi"));
-    let result = list.todo();
+    let result = list.get_all();
     println!("{:?}", result);
 }
